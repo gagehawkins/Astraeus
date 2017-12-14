a2d666a518410cbb5312011f95d8b116fe8fc475
Gage Hawkins <gagehawkins@outlook.com>
2017-12-14 15:21:30 -0800

    movement 2

--- Astraeus/Assets/CharacterMovement.cs
+++ Astraeus/Assets/CharacterMovement.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour {
+    public float speed;
+
+    public Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        float flyMode = Input.GetAxis("Submit");
+
+        //move player acording to input
+        if (flyMode == 0)
+        {
+            vertical = 0;
+            rb.gravityScale = 2;
+        }
+        else
+        {
+            rb.gravityScale = 0;
+        }
+        Vector2 movement = new Vector2(horizontal, vertical);
+
+        rb.AddForce(movement * speed);
+    }
 }
