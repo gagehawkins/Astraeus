52537dff99b511f74bb60e2bf468dfdfaa2291cc
Gage Hawkins <gagehawkins@outlook.com>
2018-01-17 08:54:06 -0800

    ELEVATORS

---
+++ Astraeus/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+    public float speed;
+    public float jumpStrength;
+
+    public Rigidbody2D rb;
+
+    LayerMask jumpCollider;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+        jumpCollider = LayerMask.NameToLayer("Middleground");
+        //Debug.Log("LayerINT = " + jumpCollider.value);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 lineStart = new Vector2(transform.position.x, transform.position.y - 1);
+        Vector2 lineEnd = new Vector2(transform.position.x, transform.position.y - 2);
+
+        Vector2 movement = new Vector2(horizontal, 0);
+        //Debug.Log(jumpCollider.value << 9);
+        //Send Linecast below character to ground
+        //RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity, Mask.value);
+        RaycastHit2D hit = Physics2D.Linecast(lineStart, lineEnd, jumpCollider.value << 9);
+
+        //Debug.DrawRay(lineStart, Vector3.down, Color.blue, 1);
+
+        //Test if Linecast hit ground
+        if (hit.collider != null && hit.collider.tag == "Jump")
+        {
+            //Debug.Log("Colliding with = " + hit.collider.name);
+            //Debug.Log("Has Tag = " + hit.collider.tag);
+            //Debug.Log("Distance = " + hit.distance);
+            //Test if player is on ground
+            if (hit.distance <= 0.05)
+            {
+                if (vertical > 0)
+                {
+                    //Make character jump
+                    rb.AddForce(Vector2.up * (jumpStrength * 100));
+                    //Debug.Log("Ground Distance = " + hit.distance);
+                }
+
+                //Move character
+                rb.AddForce(movement * speed);
+            }
+        }
+    }
+}
---
+++ Astraeus/Assets/Scripts/elevatorControl.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elevatorControl : MonoBehaviour {
+    float elevatorCooldown;
+
+    //float Distance;
+    LayerMask elevator;
+
+    // Use this for initialization
+    void Start () {
+        elevator = LayerMask.NameToLayer("Elevator");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if (elevatorCooldown != 0)
+        {
+            elevatorCooldown--;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        float elevatorControl = Input.GetAxis("elevatorControl");
+        Debug.Log(elevatorControl);
+        //float moveChange;
+
+        Vector2 rayDownStart = new Vector2(transform.position.x, transform.position.y - 2);
+        Vector2 rayDownEnd = new Vector2(transform.position.x, transform.position.y - 10);
+        Vector2 rayUpStart = new Vector2(transform.position.x, transform.position.y + 2);
+        Vector2 rayUpEnd = new Vector2(transform.position.x, transform.position.y + 10);
+        //Vector2 newElevator = new Vector2(0, 0);
+        //Debug.Log(elevator.value << 10);
+        if (elevatorControl < 0 && elevatorCooldown == 0)
+        {
+            RaycastHit2D downHit = Physics2D.Linecast(rayDownStart, rayDownEnd, elevator.value << 10);
+            if (downHit.collider != null)
+            {
+
+                Debug.Log("down");
+                Vector2 newElevator = new Vector2(downHit.transform.position.x, downHit.transform.position.y);
+                Debug.Log(newElevator);
+                collision.transform.position = newElevator;
+                elevatorCooldown = 100;
+            }
+        }
+        else if (elevatorControl > 0 && elevatorCooldown == 0)
+        {
+            RaycastHit2D upHit = Physics2D.Linecast(rayUpStart, rayUpEnd, elevator.value << 10);
+            if (upHit.collider != null)
+            {
+                Debug.Log("up");
+                Vector2 newElevator = new Vector2(upHit.transform.position.x, upHit.transform.position.y);
+                Debug.Log(newElevator);
+                collision.transform.position = newElevator;
+                elevatorCooldown = 100;
+            }
+        }
+    }
+}
